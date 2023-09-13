8eaa47409c5b91538e3de8e67b0f0903805fa45b
Muhamadfikri20 <muhamadfikri9602@gmail.com>
2023-09-13 23:51:15 +0700

    upload image api, multiple image , and upload wajib login

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
-
+use App\Http\Controllers\Api\UploadController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -25,5 +25,10 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/logout', [AuthController::class, 'logout']) 
     ->middleware('auth:sanctum');
+Route::post('image/upload', [UploadController::class, 'uploadImage'])
+    ->middleware('auth:sanctum');
+Route::post('image/upload-multiple', [UploadController::class, 'uploadMultipleImage'])
+    ->middleware('auth:sanctum');
+
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('products', ProductController::class);
